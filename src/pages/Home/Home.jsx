0688cc68e4464108cd6305c76b854874955e2c31
MOUNIER <alexandremounier14@gmail.com>
2025-03-11 20:11:42 +0100

    creation of Banner

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Header from "../../components/Header";
+import Banner from "../../components/Banner";
 
 
 export default function Home(){
     return (
         <div>
         <Header/>
-        <p>Bonjour</p>
+        <Banner/>
         </div>
     )   
 }
